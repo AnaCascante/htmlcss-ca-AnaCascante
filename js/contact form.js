e1878b0808bcffc7a2b0fd0ea42e7542f710d4bc
AnaCascante <ana.cascante@stud.noroff.no>
2022-12-18 22:41:14 +0100

    i did something more.....

--- js/contact form.js
+++ js/contact form.js
@@ -1,6 +1,32 @@
 
-const form = function (){
-    document.querySelector("name")
-    document.querySelector("email")
-    document.querySelector("message")
+
+const userName = document.querySelector("name")
+const email =  document.querySelector("email")
+const message = document.querySelector("message")
+
+userName.onsubmit (){
+    if(userName.value == ""){
+        message.innerHTML ="we are missing your name";
+    } else { 
+        message.innerHTML = ""; 
+        return true; 
+    }
+}
+
+email.onsubmit (){
+    if(userName.value == ""){
+        message.innerHTML ="we are missing your name";
+    } else { 
+        message.innerHTML = ""; 
+        return true; 
+    }
+}
+
+message.onsubmit (){
+    if(userName.value == ""){
+        message.innerHTML ="we are missing your name";
+    } else { 
+        message.innerHTML = ""; 
+        return true; 
+    }
 }
